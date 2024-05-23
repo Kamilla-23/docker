43ad7e2dfdf5bdf14383cd8dd6e27ec883a91c09
Kamilla <s_nauruzbayeva23@stud.hwr-berlin.de>
2024-05-23 17:15:04 +0200

    Changed host name for deployment

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='srv-captain--redis', port=6379)
 
 def get_hit_count():
     retries = 5
